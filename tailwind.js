1ebe1929e724099811e615cbb768d59f5edc5f06
LuisAlberto22 <guichos-mx@outlook.com>
2023-08-18 17:19:22 -0600

    Terminado

---
+++ tailwind.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+      fontFamily: {
+        'Quantico': ['Quantico'],
+        'GeostarFill': ['Geostar Fill'],
+      },
+        container: {
+            center: true,
+          },
+      extend: {
+            colors: {
+                'matrix-text': '#44F24F',
+                'matrix-text-bg': '#34Bf49',
+                'matrix-components': '#12732A',
+                'matrix-hover': '#01260A',
+                'matrix-footer' : '#1A1A1A',
+                'matrix-bg': '#0D0D0D',
+                'matrix-text-blue': '#040DBF',
+                'matrix-text-bg-blue': '#072B73',
+                'matrix-components-blue': '#030A8C',
+                'matrix-hover-blue': '#010A26',
+                'matrix-footer' : '#1A1A1A',
+                'matrix-bg-blue': '#061940',
+              },
+        
+      }
+    }
+  }
